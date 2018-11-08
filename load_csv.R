442fbc25ca78e65ad7ad0f812ace9cce3d0ed17f
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2018-11-07 19:39:57 -0500

    Playing around

---
+++ load_csv.R
@@ -0,0 +1,34 @@
+#library(leaps)
+#library(faraway)
+#library(MASS)
+
+setwd("D://Users//rsaaved//20181107")
+
+base<-read.csv(file="salario - gasto vivienda.csv",head= TRUE,sep=";")
+
+plot(base$Gasto,base$Salario)
+
+base.lm <- lm(Gasto~.,data=base)
+
+summary(base.lm)
+
+library(car)
+
+#gráfico de residuos
+plot(base.lm$model$Salario,base.lm$residuals)
+abline(0,0)
+
+#Test de homocedasticidad
+ncvTest (base.lm)
+
+#Test por la normalidad de los residuos
+
+#grafico entre cuartil teorico y estimado
+qqnorm(base.lm$residuals)
+qqline(base.lm$residuals)
+
+#test de Shapiro-Wilk 
+shapiro.test(base.lm$residuals)
+
+#test de Durbin-Watson
+durbinWatsonTest(base.lm$residuals)
