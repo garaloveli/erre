5512c53d159ceee9e51e462f54f310a8eac8cbb1
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2018-11-07 20:08:36 -0500

    Exercise five

---
+++ exercise_five.R
@@ -0,0 +1,38 @@
+setwd("~/Projects/erre/")
+
+#install.packages("readxl")
+
+#ipc_pdj<-read.xlsx("IPC-PDJ.xlsx", sheetIndex = 1)
+
+#install.packages("gdata")
+
+require(gdata)
+
+ipc_pdj <- read.xls("IPC-PDJ.xlsx", sheet = 1, header = TRUE)
+
+plot(ipc_pdj$PDJ,ipc_pdj$IPC)
+
+ipc_pdj.lm <- lm(PDJ~.,data=ipc_pdj)
+
+summary(ipc_pdj.lm)
+
+
+library(car)
+
+plot(ipc_pdj.lm$model$IPC,ipc_pdj.lm$residuals)
+abline(0,0)
+
+#Test de homocedasticidad
+ncvTest (ipc_pdj.lm)
+
+#Test por la normalidad de los residuos
+
+#grafico entre cuartil teorico y estimado
+qqnorm(ipc_pdj.lm$residuals)
+qqline(ipc_pdj.lm$residuals)
+
+#test de Shapiro-Wilk 
+shapiro.test(ipc_pdj.lm$residuals)
+
+#test de Durbin-Watson
+durbinWatsonTest(ipc_pdj.lm$residuals)
